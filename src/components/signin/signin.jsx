f89dd814f7bd008223b75e07ac5f94f03fda6f4f
Verkijikavictorine <116363697+Verkijikavictorine@users.noreply.github.com>
2023-09-08 15:12:27 +0100

    IPRO_29: signin and signup forms (#2)
    * created signin and signup forms
    
    * changes made
    
    * Background image adjusted
    
    * styles ajusted according to components
    
    * Changes,e.g component names ,styles... updated
    
    * Component name edited

---
+++ src/components/signin/signin.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import "../signin/signin.css";
+import logo1  from '../../assets/logo-preview .png';
+
+
+const SignIn = () => {
+    return ( 
+      
+      <div class="signinContainer">
+      <div class="signin-form">
+        <div class="signin-header">
+        <img class="logo" src={logo1} alt="Logo" />
+
+          <h2>Login</h2>
+        </div>
+        <form>
+          <div class="mb-3">
+            <label for="username" class="form-label"><b>Username</b></label>
+            <input type="text" class="form-control" id="username" placeholder="Enter your username"/>
+          </div>
+        
+          <div class="mb-3">
+            <label for="password" class="form-label"><b>Password</b></label>
+            <input type="password" class="form-control" id="password" placeholder="Enter your password" required/>
+          </div>
+        
+          <button type="submit" class="btn btn-light"><b>Login</b></button>
+          <Link to="/SignUp">
+                <button id="signInAccount-button">Create account</button>
+              </Link>
+        </form>
+      </div>
+      </div>
+    );
+};
+ 
+export default SignIn;
+
+
